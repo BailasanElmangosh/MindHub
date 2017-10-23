a966f5289c2ca26bcb29aa8e9f3d1a08d2c4bed4
amrghonem <amr-ghonem@hotmail.com>
2017-10-23 08:26:45 +0200

    Subcategory bug fixing

--- angularcomponents/DashBoard/Ctrl/subCategoryCtrl.js
+++ angularcomponents/DashBoard/Ctrl/subCategoryCtrl.js
@@ -1,37 +1,43 @@
 (function () {
     dashBoard.controller('subCategoryCtrl', 
     function($scope,subCategorySrv,categorysSrv) {
-        $scope.test={Name:"bailasan",MainCategoryId:15};
+   
 //Get Category
-      $scope.getCategory={};
+
       $scope.filterCondition = "hi";
+      //Ana mas7at l adima wa 3mltha tany ba nafs l tary2a asht3'let 3'yart esm l object bs
+      // wa fa ng-repeat 5leto c ma3rfsh ezay asht3'al :D 
+      $scope.MainCategories={};
       categorysSrv.categorysGet()
       .success(function(data, status){
-        $scope.getcategory= data.mainCategories;
+        $scope.MainCategories= data.mainCategories;
 
       });
 
-      $scope.subCategory={}
-   $scope.getsubCategorys={}
+//Garabt A3ml Add bl object da nfe3 wa asht3'al 
+      // $scope.subCategory={
+      //   Name:"C#",
+      //   MainCategoryId:1
+      // }
+
+      // Wa ba3d kda 3mltlo comment wa garabt mn l html 3ady a7ot l value  am asht3'al 3ady 
+
+
 
       // Add Sub Category 
       
       $scope.addSubCategory=function(){
-
         subCategorySrv.CreateSubCategory($scope.subCategory)
         .success(function(data, status){
-             alert(data.status);
-             $scope.getsubCategorys.push(data.test)
-             $scope.subCategory.Name = null;
+            console.log(data)
+             $scope.getsubCategorys.push(data.subCategory)
         });
       };
     // //  Get ALL Sub Category 
   
     subCategorySrv.GetsubCategorys()
       .success(function(data, status){
-          
         $scope.getsubCategorys= data.subCategories;
-
       });
 
 
