9ff0190a2eb9498e64610832c612ee1ca55b572c
BailasanElmangosh <Bailasan_elmangosh@hotmail.com>
2017-11-19 12:26:25 +0200

    imageProfileDefault

--- angularcomponents/studentProfile/Ctrl/studentProfileCtrl.js
+++ angularcomponents/studentProfile/Ctrl/studentProfileCtrl.js
@@ -2,7 +2,8 @@
     
     studentProfile.controller("studentProfileCtrl", function ($scope,profileDataSrv,$cookies) {
             $scope.profileData={};
-         
+            $scope.maleImg = "../../../images/default.png";
+            $scope.FemaleImg = "../../../images/defaultfemale.jpg";
             profileDataSrv.getData()
             .success(function (data, status) {
                 $scope.profileData=data.profile;
---
+++ angularcomponents/studentProfile/Srvs/defultimage.js
@@ -0,0 +1,8 @@
+
+
+studentProfile.controller("MyCtrl", function($scope) {
+//  
+  $scope.maleImg = "../../../images/default.png";
+  $scope.FemaleImg = "../../../images/defaultfemale.jpg";
+  
+});
