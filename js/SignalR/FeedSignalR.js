892fe11a23850e942841ef584295b748f51adc62
amrghonem <amr-ghonem@hotmail.com>
2017-11-27 15:24:09 +0200

    .

--- js/SignalR/FeedSignalR.js
+++ js/SignalR/FeedSignalR.js
@@ -1,11 +1,16 @@
 
 
 var mainHub = $.connection.mainHub;
-$.connection.hub.url = 'http://localhost:2656/signalr/hubs';
+$.connection.hub.url = 'http://localhost:10724/signalr/hubs';
 
 $.connection.hub.qs = { "userId": "3a48b3f6-00ba-493c-b328-9a4a8ab275c2"};
 
 mainHub.client.test = function(data){alert(data);}
+
+mainHub.client.newQuestion = function(data){
+    alert("SignalR Event");
+    console.log(data);
+}
 
 $.connection.hub.start()
 .done(function(){  });
