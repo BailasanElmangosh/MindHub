560d1243a273dd44aa2e2f1cdcfa3aef3e4cdf20
BailasanElmangosh <Bailasan_elmangosh@hotmail.com>
2018-04-17 13:41:35 +0200

    .

--- angularcomponents/landingPage/Srvs/login.js
+++ angularcomponents/landingPage/Srvs/login.js
@@ -3,11 +3,7 @@
         login: function (user) {
             return $http({
                 method: "Post",
-<<<<<<< HEAD
-                url: "http://mindhubgp1-001-site1.gtempurl.com//api/Login",
-=======
                 url: "http://mindhubgp1-001-site1.gtempurl.com/api/Login",
->>>>>>> 4784e01406ca410e6b69ff850e6472f72382391b
                 data: user,
                 headers: 
                 {
